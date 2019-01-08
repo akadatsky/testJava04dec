858ab370452c21d47c35313b08596d59755853b0
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2019-01-08 18:43:57 +0200

    add method

--- src/com/company/MainClass.java
+++ src/com/company/MainClass.java
@@ -4,15 +4,18 @@ public class MainClass {
 
     public static void main(String[] args) {
         int[] arr = {1,2,5,1,2,1};
-
         int maxIndex = 0;
+        maxIndex = getMaxIndex(arr, maxIndex);
+        System.out.println(maxIndex);
+    }
+
+    private static int getMaxIndex(int[] arr, int maxIndex) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > arr[maxIndex]) {
                 maxIndex = i;
             }
         }
-        System.out.println(maxIndex);
-
+        return maxIndex;
     }
 
 
